135901a1e77c8e623e8a838758bb579585581010
MingiKang <kmgi0312@gmail.com>
2026-02-26 15:19:47 +0900

    PostCreate.jsx Icon 변경

--- src/pages/PostCreate/PostCreate.jsx
+++ src/pages/PostCreate/PostCreate.jsx
@@ -6,7 +6,7 @@ import { uploadImage } from '../../api/auth';
 import { useAuth } from '../../context/AuthContext';
 import { getImageUrl, DEFAULT_PROFILE_IMAGE } from '../../utils/format';
 import { useEffect } from 'react';
-import ImageUploadIcon from '../../assets/icons/icon-upload.svg?react';
+import ImageIcon from '../../assets/icons/icon-image.svg?react';
 import AlertModal from '../../components/common/AlertModal';
 import Header from '../../components/common/Header';
 
@@ -272,7 +272,7 @@ const PostCreate = ({ isEdit = false }) => {
 
       {images.length < MAX_IMAGES && (
         <FloatingCameraBtn onClick={() => fileRef.current?.click()}>
-          <ImageUploadIcon width="28" height="28" fill="white" />
+          <ImageIcon width="28" height="28" />
         </FloatingCameraBtn>
       )}
 
