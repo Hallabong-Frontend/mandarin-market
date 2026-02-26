aed89ddcfb607f28885a91311a6e8665d9010278
MingiKang <kmgi0312@gmail.com>
2026-02-26 10:54:28 +0900

    카메라 아이콘 제거 및 이미지 아이콘 추가

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -6,6 +6,7 @@ import { useAuth } from '../../context/AuthContext';
 import { validateAccountname, getImageUrl } from '../../utils/format';
 import Header from '../../components/common/Header';
 import AuthInput from '../../components/common/AuthInput';
+import ImageIconSvg from '../../assets/icons/icon-image.svg';
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -67,18 +68,7 @@ const SuccessText = styled.p`
   color: ${({ theme }) => theme.colors.success};
 `;
 
-const CameraIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-    <path
-      d="M23 19C23 19.5304 22.7893 20.0391 22.4142 20.4142C22.0391 20.7893 21.5304 21 21 21H3C2.46957 21 1.96086 20.7893 1.58579 20.4142C1.21071 20.0391 1 19.5304 1 19V8C1 7.46957 1.21071 6.96086 1.58579 6.58579C1.96086 6.21071 2.46957 6 3 6H7L9 3H15L17 6H21C21.5304 6 22.0391 6.21071 22.4142 6.58579C22.7893 6.96086 23 7.46957 23 8V19Z"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <circle cx="12" cy="13" r="4" stroke="white" strokeWidth="2" />
-  </svg>
-);
+const ImageIcon = () => <img src={ImageIconSvg} alt="" width="18" height="18" />;
 
 const EditProfile = () => {
   const navigate = useNavigate();
@@ -202,7 +192,7 @@ const EditProfile = () => {
               }}
             />
             <AvatarEditBtn type="button" onClick={() => fileRef.current?.click()}>
-              <CameraIcon />
+              <ImageIcon />
             </AvatarEditBtn>
             <input
               ref={fileRef}
