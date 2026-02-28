9ab7ebd3307e14cc91047c77aecb95fbc0d8f562
seulgi626 <seulgi26b@naver.com>
2026-02-28 12:38:01 +0900

    fix(chat): ChatList 모바일 환경 버튼 테두리 노출 버그 수정 (#117)
    ChatList 페이지에서 크롬개발자 모바일 버전으로 봤을 때 삭제 버튼 테두리만 표시되는 걸 제거

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -5,8 +5,8 @@ const spin = keyframes`
 `;
 
 export const SpinnerRing = styled.div`
-  width: ${({ size }) => size || '40px'};
-  height: ${({ size }) => size || '40px'};
+  width: ${({ $size }) => $size || '40px'};
+  height: ${({ $size }) => $size || '40px'};
   border: 3px solid ${({ theme }) => theme.colors.gray200};
   border-top-color: ${({ theme }) => theme.colors.primary};
   border-radius: 50%;
@@ -17,12 +17,12 @@ const SpinnerWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: ${({ padding }) => padding || '40px 0'};
+  padding: ${({ $padding }) => $padding || '40px 0'};
 `;
 
 const Spinner = ({ size, padding }) => (
-  <SpinnerWrapper padding={padding}>
-    <SpinnerRing size={size} />
+  <SpinnerWrapper $padding={padding}>
+    <SpinnerRing $size={size} />
   </SpinnerWrapper>
 );
 
